6e1cf5d2ea3942106a1597436ccb1f89a5c5f48e
unknown <irahavoi@gmail.com>
2015-06-20 23:13:24 -0500

    Example of sorting using a lambda

--- src/com/rahavoi/behavior/param/PredicateExample.java
+++ src/com/rahavoi/behavior/param/PredicateExample.java
@@ -12,10 +12,14 @@ public class PredicateExample {
 		AppleService aService = new AppleService();
 		Apple a1 = new Apple("green", 124);
 		Apple a2 = new Apple("red", 160);
+		Apple a3 = new Apple("green", 84);
+		Apple a4 = new Apple("green", 104);
 		
 		List<Apple> apples = new ArrayList<Apple>();
 		apples.add(a1);
 		apples.add(a2);
+		apples.add(a3);
+		apples.add(a4);
 		/**
 		 * Strategy pattern: behavior is expressed through different ApplePrinter implementations.
 		 */
@@ -26,6 +30,12 @@ public class PredicateExample {
 		 */
 		List<Apple> filteredApples = aService.filter(apples, (Apple apple) -> "green".equals(apple.getColor()));
 		
+		/**
+		 * Sorting using a lambda:
+		 */
+		filteredApples.sort(
+				(Apple first, Apple second) -> first.getWeight().compareTo(second.getWeight())
+		);
 		
 		aService.prettyPrintApple(filteredApples, new AppleWeightColorPrinter());
 		
