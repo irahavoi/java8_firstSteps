294e431ebbfb5159fed7c151c61523debeafc4e6
unknown <irahavoi@gmail.com>
2015-07-09 11:09:21 -0500

    Some more Collectors examples

--- src/com/rahavoi/collectors/CollectorsExamples.java
+++ src/com/rahavoi/collectors/CollectorsExamples.java
@@ -2,7 +2,9 @@ package com.rahavoi.collectors;
 
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.IntSummaryStatistics;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
@@ -29,14 +31,52 @@ public class CollectorsExamples {
 		long howManyDishes = menu.stream().collect(Collectors.counting());
 		System.out.println("Total number of dishes: " + howManyDishes);
 		
-		//Finding the most calory-rich dish:
+		//Finding the most and the least calory-rich dishes:
 		Comparator<Dish> dishCaloriesComparator = Comparator.comparingInt(Dish::getCalories);
 		
 		Optional<Dish> maxCaloryDish = menu.stream().collect(Collectors.maxBy(dishCaloriesComparator));
+		Optional<Dish> minCaloryDish = menu.stream().collect(Collectors.minBy(dishCaloriesComparator));
 		
 		if(maxCaloryDish.isPresent()){
 			System.out.println("Maximum calories dish: " + maxCaloryDish.get().getName());
 		}
+		
+		if(minCaloryDish.isPresent()){
+			System.out.println("Minimum calory dish: " + minCaloryDish.get().getName()	);
+		}
+		
+		//Summarization:
+		int totalCalories = menu.stream().collect(Collectors.summingInt(Dish::getCalories));
+		System.out.println("If you eat all the items on the menu, you'll get " + totalCalories + " calories.");
+		
+		//Average:
+		double averageCalories = menu.stream().collect(Collectors.averagingInt(Dish::getCalories));
+		System.out.println("Average calories: " + averageCalories);
+		
+		
+		//Statistics. Yay!
+		IntSummaryStatistics menuStatistics = menu.stream().collect(Collectors.summarizingInt(Dish::getCalories));
+		System.out.println(menuStatistics);
+		
+		
+		
+		String shortMenu = menu.stream().map(Dish::getName).collect(Collectors.joining(", "));
+		System.out.println("Short menu: " + shortMenu);
+		
+		
+		//Partitioning:
+		Map<Boolean, List<Dish>> partitionedMenu =
+				menu.stream().collect(Collectors.partitioningBy(Dish::isVegetarian));
+		
+		System.out.println("Partitioned menu based on 'is vegetarian' criterion:");
+		System.out.println(partitionedMenu);
+		
+		//Filtering:
+		List<Dish> vegetarianDishes = 
+				menu.stream().filter(Dish::isVegetarian).collect(Collectors.toList());
+		
+		System.out.println("Vegetarian dishes: " + vegetarianDishes);
+		
 		
 	}
 }
